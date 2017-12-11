7b4cc8448676d410ed3ace54667ddbd024b3e98c
Stuart Moore <stuart@stuart-moore.com>
2017-12-11 14:40:34 +0000

    initial push

---
+++ ansible/ansible.sh
@@ -0,0 +1,6 @@
+sudo apt-get install libffi-dev libssl-dev -y
+sudo apt-get install python-pip -y
+sudo -H pip install --upgrade setuptools
+sudo -H pip install --upgrade pip
+sudo -H pip install pywinrm
+sudo -H pip install ansible
